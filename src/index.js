d07333ce3dc57c81fdc6862bf1b4786989f9f907
friend <friend@yourfriend.lv>
2021-08-18 20:13:03 +0300

    wowwww halal1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import express from "express"
+import fs from "fs";
+import shell from "shelljs"
+
+let config = {};
+
 const app = express();
 
 app.use(express.json());
@@ -8,10 +13,39 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-	console.log(req.body)
+	if(req?.body?.repository?.full_name) {
+		const location = config.repoToLocation.find(e => e.repo == req.body.repository.full_name);
+
+		if(location) {
+			if(fs.existsSync(location.location)) {
+				shell.exec(`git pull ${location.location}`, true)				
+				console.log("(((: Updating " + location.repo + " at " + new Date().toLocaleTimeString())
+			} else {
+				console.log("/: Couldn't find " + location.repo + " at " + location.location + ". Attempting to clone it.")
+				shell.exec(`git clone https://github.com/${location.repo} ${location.location}`, true)
+			}
+			shell.exec(location.command, true)
+		} else {
+			console.log("): Update sent from " + req.body.repository.full_name + " but couldn't find config.") 
+		}
+	} else {
+		console.log("): Receieved weird POST.")
+	}
+
 	res.send(req.body)
 })
 
 app.listen(20009, () => {
+	if(!fs.existsSync("config.json")) {
+		console.log(":/ Couldn't find config.json. Exiting.")
+		process.exit(1);
+	} else {
+		config = JSON.parse(fs.readFileSync("config.json").toString());
+	}
+
+	if (!shell.which('git')) {
+		console.log("): Couldn't find git.")
+		process.exit(1)
+	}
 	console.log(":) Gitpdate running on port 20009.")
 })
