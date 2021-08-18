f8c20149940d710e0ea66d2dbc47cf7cb0b19834
friend <friend@yourfriend.lv>
2021-08-18 20:50:07 +0300

    No clues to what this is.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Gitpdate is running! Enjoy your repos being synced!")
+	res.send(`200 OK`)
 })
 
 app.post("/", (req, res) => {
